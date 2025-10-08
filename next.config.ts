9809228c41aef3fe7d8fe97aecbc076976836d9d
whitekitten2813 <whitekitten2813@gmail.com>
2025-10-08 20:18:20 +0700

    repo next landing page

---
+++ next.config.ts
@@ -0,0 +1,19 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        hostname: "ik.imagekit.io",
+        protocol: "https",
+      },
+    ],
+  },
+};
+
+export default nextConfig;
+
+// added by create cloudflare to enable calling `getCloudflareContext()` in `next dev`
+import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare";
+initOpenNextCloudflareForDev();
